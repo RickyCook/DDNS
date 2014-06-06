6c78551631aa6156392b2ceedc92752858750a3e
Ricky Cook <mail@thatpanda.com>
2014-06-06 15:22:09 +0400

    Add database settings

--- ddns/private_settings.example.py
+++ ddns/private_settings.example.py
@@ -1,3 +1,12 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'secret'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thatpanda_ddns',
+        'USER': 'thatpanda_ddns',
+        'PASSWORD': '',
+        'HOST': '/var/run/postgresql',
+    }
+}
